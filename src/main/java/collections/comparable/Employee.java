63b6a5b46d10d528e9172827f9967b220cd0876a
JK <jayakrishnan.b@outlook.com>
2019-11-21 01:55:17 +0530

    EOD commit override

--- src/main/java/collections/comparable/Employee.java
+++ src/main/java/collections/comparable/Employee.java
@@ -1,4 +1,26 @@
 package collections.comparable;
 
 public class Employee {
+
+    private String empFirstname;
+    private String empLastname;
+    private int empID;
+
+    public Employee(String empFirstname, String empLastname, int empID) {
+        this.empFirstname = empFirstname;
+        this.empLastname = empLastname;
+        this.empID = empID;
+    }
+
+    public String getEmpFirstname() {
+        return empFirstname;
+    }
+
+    public String getEmpLastname() {
+        return empLastname;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
 }
--- src/main/java/collections/comparable/Runner.java
+++ src/main/java/collections/comparable/Runner.java
@@ -1,4 +1,19 @@
 package collections.comparable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
+
+    public static void main(String[] args){
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Hari", "Krishnan", 125444));
+        employeeList.add(new Employee("Sam", "Broddy", 125888));
+
+        for(Employee l : employeeList){
+            System.out.println(l);
+        }
+
+    }
 }
